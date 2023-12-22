eb8f2b2c03c9d7d333118983cbf01cb2a1ad4b42
+collins <+collinskipngetich60@gmail.com>
2023-12-22 12:47:15 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+// index.js
+
+// Set companyName as 'Scuber'
+let companyName = 'Scuber';
+
+// Make companyName a constant
+const companyNameConst = 'Scuber';
+
+// Declare and initialize mostProfitableNeighborhood using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Declare and initialize companyCeo using let
+let companyCeo = 'Susan Smith';
+
+
+// make the companyceo a constant
+const companyCeoConst= 'Susan Smith';
